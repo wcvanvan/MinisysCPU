4ba0d714bba33bd7a76e4f48db123c01bf531de7
wcvanvan <1057736842@qq.com>
2022-05-30 17:22:13 +0800

    update

--- sources_1/ip/prgrom/prgrom_stub.v
+++ sources_1/ip/prgrom/prgrom_stub.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
-// Date        : Mon May 30 15:32:58 2022
+// Date        : Mon May 30 16:49:13 2022
 // Host        : DESKTOP-V9NBHV8 running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub
 //               D:/Projects/cpu/sccpuv0.1.0/sccpu.srcs/sources_1/ip/prgrom/prgrom_stub.v
